0efbe0e0f6c463cb1806a97c7743f3def34531d0
Sanskar005 <sanskar535mohanty@gmail.com>
2023-06-22 21:43:33 +0530

    Updated favorites, recipe, footer

--- Client/app_recipe/src/App.js
+++ Client/app_recipe/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import Recipe from "./Pages/Recipe/Recipe";
+import Favourites from "./Pages/Favourites/Favourites";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-
+      <Recipe />
+      {/* <Favourites /> */}
+      {/* <Footer/> */}
     </div>
   );
 }
